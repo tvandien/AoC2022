a3d1b6f9c366e47f943cb2ccfa1e4869c8064319
Tijmen van Dien <tijmenv@ndien.nl>
2022-12-01 10:52:48 +0100

    Add day 1

--- AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,15 +2,26 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2022, "") { }
+    public Solution() : base(01, 2022, "Calorie Counting", false) { }
+
+    private List<int> ParseInput() => Input
+        .SplitByParagraph()
+        .Select(singleElf => singleElf.ToIntArray("\n").Sum())
+        .ToList();
 
     protected override string SolvePartOne()
     {
-        return "";
+        var caloriesPerElf = ParseInput();
+
+        return caloriesPerElf.Max().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        List<int> caloriesPerElf = ParseInput();
+
+        caloriesPerElf.Sort();
+
+        return caloriesPerElf.TakeLast(3).Sum().ToString();
     }
 }
