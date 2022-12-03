26077ce8d4bbb86a8c0e8ae1c2287912a90a9f19
Tijmen van Dien <tijmenv@ndien.nl>
2022-12-03 10:08:52 +0100

    Add day 3

--- AdventOfCode.Solutions/Year2022/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day03/Solution.cs
@@ -2,15 +2,72 @@ namespace AdventOfCode.Solutions.Year2022.Day03;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(03, 2022, "") { }
+    public Solution() : base(03, 2022, "Rucksack Reorganization", false) { }
+
+    public class ElfGroup
+    {
+        public string[] Rucksacks { get; set; }
+
+        public ElfGroup(string[] rucksacks) 
+        {
+            Rucksacks = rucksacks;
+        }
+
+        public char FindOverlappingItem()
+        {
+            return Rucksacks[0]
+                .First(a => Rucksacks[1].Contains(a) && Rucksacks[2].Contains(a));
+        }
+    }
+
+    public class Rucksack
+    {
+        public string CompartmentOne { get; set; }
+        public string CompartmentTwo { get; set; }
+
+        public Rucksack(string contents)
+        {
+            CompartmentOne = contents[..(contents.Length / 2)];
+            CompartmentTwo = contents[(contents.Length / 2)..];
+        }
+
+        public char FindOverlappingItem()
+        {
+            return CompartmentOne.First(a => CompartmentTwo.Contains(a));
+        }
+    }
+
+    public static int ItemValue(char item)
+    {
+        return item switch
+        {
+            >= 'A' and <= 'Z' => item - 'A' + 27,
+            >= 'a' and <= 'z' => item - 'a' + 1,
+            _ => throw new Exception($"Invalid item: {item}")
+        };
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return Input
+            .SplitByNewline()
+            .Select(a => new Rucksack(a))
+            .Select(a => a.FindOverlappingItem())
+            .Select(a => ItemValue(a))
+            .Sum()
+            .ToString();
+
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return Input
+            .SplitByNewline()
+            .Chunk(3)
+            .Select(a => new ElfGroup(a))
+            .Select(a => a.FindOverlappingItem())
+            .Select(a => ItemValue(a))
+            .Sum()
+            .ToString();
     }
 }
