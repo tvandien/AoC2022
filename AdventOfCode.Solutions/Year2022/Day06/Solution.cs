b832d462539293f3034eca1ab60e8367058023e9
Tijmen van Dien <tijmenv@ndien.nl>
2022-12-06 08:34:15 +0100

    Add day 6

--- AdventOfCode.Solutions/Year2022/Day06/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day06/Solution.cs
@@ -2,15 +2,26 @@ namespace AdventOfCode.Solutions.Year2022.Day06;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(06, 2022, "") { }
+    public Solution() : base(06, 2022, "Tuning Trouble", false) { }
 
+    private static int FindGroupOfDistinctCharacters(string input, int distinctCharacterCount)
+    {
+        for (int i = 0; i < input.Length - distinctCharacterCount; i++)
+        {
+            if (input.Substring(i, distinctCharacterCount).Distinct().Count() == distinctCharacterCount)
+            {
+                return i + distinctCharacterCount;
+            }
+        }
+        return -1;
+    }
     protected override string SolvePartOne()
     {
-        return "";
+        return FindGroupOfDistinctCharacters(Input, 4).ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return FindGroupOfDistinctCharacters(Input, 14).ToString();
     }
 }
