9abd277d614510c7d1ed0c946f3137180487a2ef
Tijmen van Dien <tijmenv@ndien.nl>
2022-12-10 13:16:11 +0100

    Add day 10

--- AdventOfCode.Solutions/Year2022/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day10/Solution.cs
@@ -2,15 +2,92 @@ namespace AdventOfCode.Solutions.Year2022.Day10;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(10, 2022, "") { }
+    public Solution() : base(10, 2022, "Cathode-Ray Tube", false) { }
+
+    public class Tubes
+    {
+        public List<long> RegisterX = new();
+        public Tubes() 
+        {
+            RegisterX.Add(1);
+        }
+
+        public void ProcessInput(string[] input)
+        {
+            foreach (var line in input)
+            {
+                switch (line[0..4])
+                {
+                    case "noop":
+                        RegisterX.Add(RegisterX.Last());
+                        break;
+                    case "addx":
+                        RegisterX.Add(RegisterX.Last());
+                        RegisterX.Add(RegisterX.Last() + GetIncrementFromAddXLine(line));
+                        break;
+                    default:
+                        throw new Exception($"Unknown command: {line}");
+                }
+            }
+        }
+
+        private static long GetIncrementFromAddXLine(string input)
+        {
+            return long.Parse(input.Split(" ")[1]);
+        }
+
+        public long GetSignalStrengthAtCycle(int cycle)
+        {
+            return RegisterX[cycle - 1] * cycle;
+        }
+
+        public long GetSumOfSignalStrengths()
+        {
+            long result = 0;
+
+            for (int c = 20; c <= 220; c+=40)
+            {
+                result += GetSignalStrengthAtCycle(c);
+            }
+
+            return result;
+        }
+
+        public string ComputeImage()
+        {
+            string output = "\r\n";
+            
+            for (int row = 0; row < 6; row++)
+            {
+                for (int column = 0; column < 40; column++)
+                {
+                    if (Math.Abs(RegisterX[row * 40 + column] - column) <= 1)
+                    {
+                        output += "#";
+                    }
+                    else
+                    {
+                        output += " ";
+                    }
+                }
+                output += "\r\n";
+            }
+
+            return output;
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var tubes = new Tubes();
+        tubes.ProcessInput(Input.SplitByNewline());
+        return tubes.GetSumOfSignalStrengths().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var tubes = new Tubes();
+        tubes.ProcessInput(Input.SplitByNewline());
+        return tubes.ComputeImage();
     }
 }
