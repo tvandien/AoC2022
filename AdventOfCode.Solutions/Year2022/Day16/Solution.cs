d5c1c3ee9f05248e3f2147dbf708b961fcf7f6b5
Tijmen van Dien <tijmenv@ndien.nl>
2023-02-20 18:33:56 +0100

    add day 16a and 17a

--- AdventOfCode.Solutions/Year2022/Day16/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day16/Solution.cs
@@ -1,12 +1,233 @@
+using System;
+using System.Reflection;
+
 namespace AdventOfCode.Solutions.Year2022.Day16;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(16, 2022, "") { }
+    public Solution() : base(16, 2022, "Proboscidea Volcanium", false) { }
+
+
+
+    public class Node
+    {
+        public string Name { get; set; }
+        public int Rate { get; set; }
+        public List<Edge> Edges { get; set; } = new();
+        public int Distance { get; set; } = 0;
+        public int Delay { get; set; } = 1;
+        public int ActivationDelay { get; set; } = 1;
+
+        public Node(string name, int rate)
+        {
+            Name = name;
+            Rate = rate;
+        }
+
+        public List<Node> GetNeighbors()
+        {
+            return Edges
+                .Select(a => a.A.Name == Name ? a.B : a.A)
+                .ToList();
+        }
+
+        public void RemoveEdgeToNode(Node node, Graph graph)
+        {
+            Edges.RemoveAll(a => (a.A.Name == node.Name || a.B.Name == node.Name));
+            graph.Edges.RemoveAll(a => (a.A.Name == node.Name || a.B.Name == node.Name));
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} <-> {string.Join(", ", GetNeighbors().Select(a => $"{a.Name}"))} ({Rate})";
+        }
+    }
+
+    public class Edge
+    {
+        public Node A { get; set; }
+        public Node B { get; set; }
+        public int Weight { get; set; }
+        public bool Bidirectional { get; set; }
+
+        public Edge(Node a, Node b, int weight = 1, bool bidirectional = false)
+        {
+            A = a;
+            B = b;
+            Weight = weight;
+            Bidirectional = bidirectional;
+        }
+
+        public override string ToString()
+        {
+            return $"{A.Name} {(Bidirectional ? "<->" : "->")} {B.Name}";
+        }
+    }
+
+    public class Graph
+    {
+        public List<Edge> Edges { get; set; } = new();
+
+        public Dictionary<string, Node> Nodes { get; set; } = new();
+
+        private Node SelectOrCreateNode(string name, int rate)
+        {
+            if (Nodes.TryGetValue(name, out Node? node))
+            {
+                if (node.Rate == -1)
+                {
+                    node.Rate = rate;
+                }
+                return node;
+            }
+
+            var newNode = new Node(name, rate);
+            Nodes.Add(name, newNode);
+
+            return newNode;
+        }
+
+        private static void ParseLine(string line, out string name, out int rate, out string[] neighbours)
+        {
+            var words = line
+                .Replace("="," ")
+                .Replace(";", "")
+                .Split(" ");
+            name = words[1];
+            rate = int.Parse(words[5]);
+            neighbours = string
+                .Join("", words[10..])
+                .Split(",");
+        }
+
+        public Graph(string[] input)
+        {
+            foreach(var line in input)
+            {
+                ParseLine(line, out string name, out int rate, out string[] neighbours);
+
+                var currentNode = SelectOrCreateNode(name, rate);
+
+                foreach (var neighbour in neighbours)
+                {
+                    var neighbourNode = SelectOrCreateNode(neighbour, -1);
+
+                    if (neighbourNode.GetNeighbors().Any(a => a.Name == currentNode.Name)) continue;
+
+                    var edge = new Edge(currentNode, neighbourNode, 1, true);
+                    currentNode.Edges.Add(edge);
+                    neighbourNode.Edges.Add(edge);
+                    Edges.Add(edge);
+                }
+            }
+
+            Reduce();
+        }
+
+        public void Reduce()
+        {
+            foreach (var node in Nodes.Values)
+            {
+                if (node.Rate > 0) continue;
+                if (node.Edges.Count > 2) continue;
+
+                var neighbours = node.GetNeighbors().ToList();
+
+                var newEdge = new Edge(neighbours[0], neighbours[1], node.Edges.Sum(a => a.Weight));
+
+                neighbours[0].RemoveEdgeToNode(node, this);
+                neighbours[1].RemoveEdgeToNode(node, this);
+                neighbours[0].Edges.Add(newEdge);
+                neighbours[1].Edges.Add(newEdge);
+
+                Nodes.Remove(node.Name);
+            }
+        }
+
+        private Dictionary<int, string> CopyAndAdd(Dictionary<int, string> input, string toAdd, int atTime)
+        {
+            var output = new Dictionary<int, string>();
+            foreach (var (key, val) in input)
+            {
+                output[key] = val;
+            }
+            output[atTime] = toAdd;
+
+            return output;
+        }
+
+        public Dictionary<int, string> FindBestSolution(Dictionary<int, string> minutes, Dictionary<int, string> active, string lastLocation, int time)
+        {
+            if (time > 10)
+            {
+                int score = active.Sum(a => (30 - a.Key) * Nodes[a.Value].Rate);
+
+                if (score < 400)
+                {
+                    return active;
+                }
+            }
+            if (time >= 30)
+            {
+                return active;
+            }
+
+            string location = minutes[time];
+            var node = Nodes[location];
+            List<Dictionary<int, string>> options = new();
+
+            if (node.Rate > 0 && !active.ContainsValue(location))
+            {
+                options.Add(
+                    FindBestSolution(
+                        CopyAndAdd(minutes, location, time + 1),
+                        CopyAndAdd(active, location, time),
+                        location,
+                        time + 1)
+                    );
+            }
+
+            var paths = node.GetNeighbors();
+
+            foreach (var path in paths)
+            {
+                if (path.Name == lastLocation) continue;
+                var edge = path.Edges.First(a => a.A.Name == path.Name ? a.B.Name == location : a.A.Name == location);
+
+                options.Add(
+                    FindBestSolution(
+                        CopyAndAdd(minutes, path.Name, time + edge.Weight),
+                        active,
+                        location,
+                        time + edge.Weight)
+                    );
+            }
+
+            if (!options.Any())
+            {
+                return active;
+            }
+
+            var scores = options.Select(a => a.Sum(a => (30 - a.Key) * Nodes[a.Value].Rate)).ToList();
+            var index = scores.IndexOf(scores.Max());
+
+            return options[index];
+        }
+    }
+
 
     protected override string SolvePartOne()
     {
-        return "";
+        var graph = new Graph(Input.SplitByNewline());
+        var start = new Dictionary<int, string>
+        {
+            { 1, "AA" }
+        };
+        var result = graph.FindBestSolution(start, new Dictionary<int, string>(), "", 1);
+
+        var score = result.Sum(a => (30 - a.Key) * graph.Nodes[a.Value].Rate);
+
+        return score.ToString();
     }
 
     protected override string SolvePartTwo()
--- AdventOfCode.Solutions/Year2022/Day17/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day17/Solution.cs
@@ -2,15 +2,323 @@ namespace AdventOfCode.Solutions.Year2022.Day17;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(17, 2022, "") { }
+    public Solution() : base(17, 2022, "Pyroclastic Flow", true) { }
+
+    private readonly int[,,] Shapes = new int[,,]
+    {
+        {
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 1, 1, 1, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        },
+        {
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 1, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 1, 1, 0, 0, 2 },
+            { 2, 0, 0, 0, 1, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        },
+        {
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 1, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 1, 0, 0, 2 },
+            { 2, 0, 0, 1, 1, 1, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        },
+        {
+            { 2, 0, 0, 1, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        },
+        {
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 1, 0, 0, 0, 2 },
+            { 2, 0, 0, 1, 1, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        },
+    };
+
+    private int[,] ShapeCoordinates = new int[,]
+    {
+        // x, y, width, height
+        { 3, 3, 4, 1 },
+        { 3, 1, 3, 3 },
+        { 3, 1, 3, 3 },
+        { 3, 0, 1, 4 },
+        { 3, 2, 2, 2 },
+    };
+    int[,] game;
+    int[,] protoGame =
+    {
+        { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        { 2, 0, 0, 0, 0, 0, 0, 0, 2 },
+        { 4, 3, 3, 3, 3, 3, 3, 3, 4 },
+    };
+
+    int shapeX, shapeY, shapeWidth, shapeHeight;
+
+    int pruned = 0;
+
+    private int FindFirstNonEmptyRow()
+    {
+        for (int row = 0; row < game.GetLength(0); row++)
+        {
+            for (int column = 1; column < game.GetLength(1) - 1; column++)
+            {
+                if (game[row, column] > 0)
+                {
+                    return row;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private void Prune(int limit = 1000, int toPrune = 900)
+    {
+        if (game.GetLength(0) > limit)
+        {
+            var newGame = new int[game.GetLength(0) - toPrune, game.GetLength(1)];
+            for (int row = 0; row < game.GetLength(0) - toPrune; row++)
+            {
+                for (int column = 0; column < game.GetLength(1); column++)
+                {
+                    newGame[row, column] = game[row, column];
+                }
+            }
+            game = newGame;
+            pruned += toPrune;
+        }
+    }
+
+    private void RemoveEmptyRows()
+    {
+        var firstNonEmptyRow = FindFirstNonEmptyRow();
+
+        if (firstNonEmptyRow > 0)
+        {
+            var newGame = new int[game.GetLength(0) - firstNonEmptyRow, game.GetLength(1)];
+            for (int row = firstNonEmptyRow; row < game.GetLength(0); row++)
+            {
+                for (int column = 0; column < game.GetLength(1); column++)
+                {
+                    newGame[row - firstNonEmptyRow, column] = game[row, column];
+                }
+            }
+            game = newGame;
+        }
+    }
+
+    int shapeIndex = 0;
+    private int[,] GetNextShape()
+    {
+        var shape = new int[Shapes.GetLength(1), Shapes.GetLength(2)];
+
+        for (int row = 0; row < shape.GetLength(0); row++)
+        {
+            for (int column = 0; column < shape.GetLength(1); column++)
+            {
+                shape[row, column] = Shapes[shapeIndex, row, column];
+            }
+        }
+
+
+        shapeX = ShapeCoordinates[shapeIndex, 0];
+        shapeY = ShapeCoordinates[shapeIndex, 1];
+        shapeWidth = ShapeCoordinates[shapeIndex, 2];
+        shapeHeight = ShapeCoordinates[shapeIndex, 3];
+
+        shapeIndex = (shapeIndex + 1) % Shapes.GetLength(0);
+
+        return shape;
+    }
+
+    private void PrependShape(int[,] shape)
+    {
+        var newGame = new int[game.GetLength(0) + shape.GetLength(0), game.GetLength(1)];
+
+        for (int row = 0; row < shape.GetLength(0); row++)
+        {
+            for (int column = 0; column < shape.GetLength(1); column++)
+            {
+                newGame[row, column] = shape[row, column];
+            }
+        }
+
+        for (int row = 0; row < game.GetLength(0); row++)
+        {
+            for (int column = 0; column < game.GetLength(1); column++)
+            {
+                newGame[row + shape.GetLength(0), column] = game[row, column];
+            }
+        }
+
+        game = newGame;
+    }
+
+    private bool CanSafelyMoveInDirection(int dy, int dx)
+    {
+        for (int row = 0; row < shapeHeight; row++)
+        {
+            for (int column = 0; column < shapeWidth; column++)
+            {
+                if (game[row + shapeY, column + shapeX] == 1)
+                {
+                    if (game[row + shapeY + dy, column + shapeX + dx] > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private void Move(int dy, int dx)
+    {
+        var shape = new int[shapeHeight, shapeWidth];
+        for (int row = 0; row < shapeHeight; row++)
+        {
+            for (int column = 0; column < shapeWidth; column++)
+            {
+                if (game[row + shapeY, column + shapeX] == 1)
+                {
+                    shape[row, column] = 1;
+                    game[row + shapeY, column + shapeX]--;
+                }
+            }
+        }
+
+        for (int row = 0; row < shapeHeight; row++)
+        {
+            for (int column = 0; column < shapeWidth; column++)
+            {
+                game[row + shapeY + dy, column + shapeX + dx] += shape[row, column];
+            }
+        }
+        shapeX += dx;
+        shapeY += dy;
+    }
+
+    private void FinalizeLocation()
+    {
+        for (int row = 0; row < shapeHeight; row++)
+        {
+            for (int column = 0; column < shapeWidth; column++)
+            {
+                if (game[row + shapeY, column + shapeX] == 1)
+                {
+                    game[row + shapeY, column + shapeX] = 5;
+                }
+            }
+        }
+    }
+
+    public void PrintGame()
+    {
+        for (int row = 0; row < game.GetLength(0); row++)
+        {
+            string output = "";
+            for (int column = 0; column < game.GetLength(1); column++)
+            {
+                output += game[row, column] switch
+                {
+                    0 => '.',
+                    1 => '@',
+                    2 => '|',
+                    3 => '_',
+                    4 => '+',
+                    5 => "#",
+                    _ => throw new Exception($"Unable to parse game at ({column}, {row}): {game[row, column]})")
+                };
+            }
+            Console.WriteLine(output);
+        }
+        Console.WriteLine();
+    }
+
+    List<long> gameSizeAfterNBlocks = new();
+    public void Simulate(string input, int count)
+    {
+        game = protoGame;
+
+        int inputIndex = 0;
+        for (int i = 0; i < count; i++) {
+            //PrintGame();
+            RemoveEmptyRows();
+            //Prune();
+            gameSizeAfterNBlocks.Add(game.GetLength(0) - 1 + pruned);
+            PrependShape(GetNextShape());
+            //PrintGame();
+
+            while (true)
+            {
+                var dx = input[inputIndex] switch
+                {
+                    '<' => -1,
+                    '>' => 1,
+                    _ => throw new Exception($"Unknown input symbol: {input[inputIndex]}")
+                };
+
+                if (CanSafelyMoveInDirection(0, dx))
+                {
+                    Move(0, dx);
+                }
+                //PrintGame();
+                inputIndex = (inputIndex + 1) % input.Length;
+
+                if (CanSafelyMoveInDirection(1, 0))
+                {
+                    Move(1, 0);
+                    //PrintGame();
+                } else
+                {
+                    FinalizeLocation();
+                    break;
+                }
+            }
+        }
+
+    }
 
     protected override string SolvePartOne()
     {
+        //Simulate(Input.Trim(), 2022);
+        //RemoveEmptyRows();
+        //return (game.GetLength(0) - 1).ToString();
         return "";
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        Simulate(Input.Trim(), 10_000);
+        RemoveEmptyRows();
+        var result2 = (game.GetLength(0) - 1);
+
+        StringBuilder sb = new();
+
+        for (int i = 0; i < gameSizeAfterNBlocks.Count; i++)
+        {
+            sb.Append($"{i}\t{gameSizeAfterNBlocks[i]}\r\n");
+        }
+
+        return $"2: {result2}";
     }
 }
